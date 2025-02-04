d1c1de771a6c8f4bc108960ef2d97faa443174f8
yann <yann.enjoyworld@gmail.com>
2025-02-04 20:48:05 +0900

    feat : solve exercises/02_basic_calculator

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -9,6 +9,9 @@
 // `factorial(2)` to return `2`, and so on.
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
+fn factorial(n: u32) -> u32 {
+    (2..=n).fold(1, |acc, i| acc * i)
+}
 
 #[cfg(test)]
 mod tests {
--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,14 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result = 1;
+    let mut n = n;
+    while 1 < n {
+        result *= n;
+        n -= 1;
+    }
+    result
+    // todo!()
 }
 
 #[cfg(test)]
