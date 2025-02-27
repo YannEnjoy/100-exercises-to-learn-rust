7b8183cb01673bf293a00e069e58d235bfe9129e
yann <yann.enjoyworld@gmail.com>
2025-02-27 15:51:38 +0900

    wip : solve exercises/06_ticket_management

--- exercises/06_ticket_management/01_arrays/src/lib.rs
+++ exercises/06_ticket_management/01_arrays/src/lib.rs
@@ -1,9 +1,10 @@
 // TODO: Flesh out the `WeekTemperatures` struct and its method implementations to pass the tests.
 
-pub struct WeekTemperatures {
-    // TODO
-}
+use std::collections::HashMap;
 
+pub struct WeekTemperatures(HashMap<Weekday, i32>);
+
+#[derive(Hash, PartialEq, Eq)]
 pub enum Weekday {
     Monday,
     Tuesday,
@@ -16,15 +17,15 @@ pub enum Weekday {
 
 impl WeekTemperatures {
     pub fn new() -> Self {
-        todo!()
+        Self(HashMap::new())
     }
 
     pub fn get_temperature(&self, day: Weekday) -> Option<i32> {
-        todo!()
+        self.0.get(&day).copied()
     }
 
     pub fn set_temperature(&mut self, day: Weekday, temperature: i32) {
-        todo!()
+        self.0.insert(day, temperature);
     }
 }
 
--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,18 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    if n == 0 {
+        return 0;
+    }
+    let (mut a, mut b) = (0, 1);
+
+    for _ in 2..=n {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    b
 }
 
 #[cfg(test)]
